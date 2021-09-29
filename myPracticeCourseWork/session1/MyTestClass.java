5ae6735dbdaa3b70007a1708ec327ba0171c4e57
ismailkefokeris <ismailkefokeris@gmail.com>
2021-09-29 16:14:06 +0100

    Session 1 Work

---
+++ myPracticeCourseWork/session1/MyTestClass.java
@@ -0,0 +1,19 @@
+
+public class MyTestClass {
+
+	public static void main(String[] args) {
+	
+	MyTestClass myTestClass = new MyTestClass();
+	myTestClass.writeAboutMe();
+	
+	}
+
+	
+	public void writeAboutMe() {
+		
+		System.out.println("I am Running the: " + MyTestClass.class);
+	}
+	
+	
+
+}
