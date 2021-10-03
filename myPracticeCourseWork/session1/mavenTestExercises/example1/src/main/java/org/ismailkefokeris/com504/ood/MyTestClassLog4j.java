34c11fb7fd96cde294c1ccbd4a91f8ddd27821ab
ismailkefokeris <ismailkefokeris@gmail.com>
2021-10-03 13:25:11 +0100

    Completed example1 (Maven)

---
+++ myPracticeCourseWork/session1/mavenTestExercises/example1/src/main/java/org/ismailkefokeris/com504/ood/MyTestClassLog4j.java
@@ -0,0 +1,24 @@
+package org.ismailkefokeris.com504.ood;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
+public class MyTestClassLog4j {
+	
+	public static Logger logger = LogManager.getLogger(MyTestClassLog4j.class);
+
+	public static void main(String[] args) {
+		
+		MyTestClassLog4j myTestClassLog4j = new MyTestClassLog4j();
+		myTestClassLog4j.writeAboutMe();
+	}
+
+
+
+	public void writeAboutMe() {
+		
+		System.out.println("This is a system out message from: " + MyTestClassLog4j.class);
+		logger.error("This is a Log4j message from: " + MyTestClassLog4j.class);
+	}
+}
---
+++ myPracticeCourseWork/session1/mavenTestExercises/example1/src/test/java/org/ismailkefokeris/com504/ood/MyTestClassLog4jTest.java
@@ -0,0 +1,17 @@
+package org.ismailkefokeris.com504.ood;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+
+public class MyTestClassLog4jTest {
+    
+    @Test
+    public void writeAboutMeTest()
+    {
+        MyTestClassLog4j testclass = new MyTestClassLog4j();
+        testclass.writeAboutMe();
+    }
+}
